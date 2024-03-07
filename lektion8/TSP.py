d34113a0d1882d4c7c7e8b4832f9e98d4c6f934c
AndersMikkelsen <ande077h@gmail.com>
2024-03-07 21:35:52 +0100

    exercises

--- lektion8/TSP.py
+++ lektion8/TSP.py
@@ -3,183 +3,151 @@ Created on Wed Mar 3 16:39:18 2021
 @author: Sila
 """
 
-# Gettting started methods for TSP GA algorithm
-# - Read cities from file
-#
-
 import pandas as pd
 import random
 import math
-
-data = pd.read_csv(
-    r'C:\Users\Bruger\Desktop\4.Semester\MachineLearning\lektion8\TSPcities1000.txt',
-    sep='\s+', header=None)
-data = pd.DataFrame(data)
-
 import matplotlib.pyplot as plt
 
-x = data[1]
-y = data[2]
+
+# Reading cities from file with pandas
+def read_cities_data(filename):
+    cities_data = pd.read_csv(filename, sep='\s+', header=None)
+    cities_data = pd.DataFrame(cities_data)
+    return cities_data
 
 
-# plt.plot(x, y,'r.')
-# plt.show()
+# Creating a random route that travels through all cities. Shuffles the order of the cities
+def generate_random_route(number_of_cities):
+    route = [[i] for i in range(number_of_cities)]
+    random.shuffle(route)
+    return route
 
 
-def createRandomRoute():
-    tour = [[i] for i in range(1000)]
-    random.shuffle(tour)
-    return tour
+# Plotting the route in 2D, with matplotlib. Gets the x and y coordinates of the cities
+def plot_city_route(route, x_coords, y_coords):
+    plt.figure()
+    for i in range(len(route) - 1):
+        x_coordinates = [x_coords[route[i]], x_coords[route[i + 1]]]
+        y_coordinates = [y_coords[route[i]], y_coords[route[i + 1]]]
+        plt.plot(x_coordinates, y_coordinates, 'ro-')
 
 
-# plot the tour - Adjust range 0..len, if you want to plot only a part of the tour.
-def plotCityRoute(route, start, end):
-    for i in range(start, end):
-        plt.plot(x[i:i + 2], y[i:i + 2], 'ro-')
-    plt.show()
-
-
-# Alternativ kode:
-#  for i in range(0, len(route)-1):
-#     plt.plot([x[route[i]],x[route[i+1]]], [y[route[i]],y[route[i+1]]], 'ro-')
-
-# calculate distance between cities
-def distancebetweenCities(city1x, city1y, city2x, city2y):
-    xDistance = abs(city1x - city2x)
-    yDistance = abs(city1y - city2y)
-    distance = math.sqrt((xDistance ** 2) + (yDistance ** 2))
+# Calculating the distance between two cities given coordinates
+def calculate_distance(city1_x, city1_y, city2_x, city2_y):
+    x_distance = abs(city1_x - city2_x)
+    y_distance = abs(city1_y - city2_y)
+    distance = math.sqrt((x_distance ** 2) + (y_distance ** 2))
     return distance
 
 
-def calculateTotalDistance(route):
-    totalDistance = 0
-    for i in range(0, number_of_citites - 1):
+# Calculating total distance of a route, by summing the distances between all cities
+def calculate_total_distance(route, x_coords, y_coords):
+    total_distance = 0
+    for i in range(len(route) - 1):
         city1 = route[i][0]
         city2 = route[i + 1][0]
-        totalDistance += distancebetweenCities(x[city1], y[city1], x[city2], y[city2])
-    return totalDistance
+        total_distance += calculate_distance(x_coords[city1], y_coords[city1], x_coords[city2], y_coords[city2])
+    return total_distance
 
 
+# Finding elite routes. Finds the best route in the given generation, based on fitness.
 def find_elites(generation, percentage):
-    sorted_gen = sorted(generation, key=calculateTotalDistance)  # no reverse because we want the lowest distance
-    return sorted_gen[:math.floor(len(sorted_gen) * percentage)]
+    sorted_generation = sorted(generation, key=lambda x: calculate_total_distance(x, x_coords, y_coords))
+    return sorted_generation[:math.floor(len(sorted_generation) * percentage)]
 
 
-def mutate(route):
-    # two random indices:
-    mut_pos1 = random.randint(0, number_of_citites - 1)
-    mut_pos2 = random.randint(0, number_of_citites - 1)
-
-    # if they're the same, skip to the chase
+# Mutating a route. Selects two random positions in the route and swaps the cities at those positions
+def mutate_route(route):
+    mut_pos1 = random.randint(0, len(route) - 1)
+    mut_pos2 = random.randint(0, len(route) - 1)
     if mut_pos1 == mut_pos2:
         return route
-
-    # Otherwise swap them:
-    city1 = route[mut_pos1]
-    city2 = route[mut_pos2]
-
-    route[mut_pos2] = city1
-    route[mut_pos1] = city2
+    route[mut_pos1], route[mut_pos2] = route[mut_pos2], route[mut_pos1]
 
 
-def breed(mum, dad):
-    child = [None] * number_of_citites
-    # fill with mum routes
-    start_at = random.randint(0, number_of_citites - 1)
-    for i in range(start_at, number_of_citites):
-        child[i] = mum[i]
-
-    # fill rest with dad routes
-    j = 0
-    for i in range(0, number_of_citites):
-        if child[i] == None:
-            while dad[j] in child:
-                j += 1
-            child[i] = dad[j]
-            j += 1
-
-    if random.random() < 0.1:
-        mutate(child)
-
-    return child
-
-
-def breed2(mum, dad):
-    start = random.randint(0, number_of_citites - 1)
-    end = random.randint(0, number_of_citites - 1)
+# Breeding routes
+def breed_routes(parent1, parent2):
+    start = random.randint(0, len(parent1) - 1)
+    end = random.randint(0, len(parent1) - 1)
 
     while start == end:
-        end = random.randint(0, number_of_citites - 1)
+        end = random.randint(0, len(parent1) - 1)
 
     if start > end:
         start, end = end, start
 
-    child = mum[start:end]
+    child = parent1[start:end]
+    remaining_cities = [city for city in parent2 if city not in child]
+    child.extend(remaining_cities)
 
-    remaining_citites = [city for city in dad if city not in child]
-    child.extend(remaining_citites)
+    if random.random() < 0.1:
+        mutate_route(child)
 
     return child
 
 
+# Performing crossover. Takes elite routes from the current generation and breeds them to create the next generation
 def crossover(generation):
     elites = find_elites(generation, 0.1)
-    number_elites = len(elites)
-    # next_gen = elites
-    next_gen = []
+    next_generation = []
 
-    for i in range(len(generation)):
-        mum = elites[random.randint(0, number_elites - 1)]
-        dad = elites[random.randint(0, number_elites - 1)]
-        next_gen.append(breed2(mum, dad))
-    return next_gen
+    for _ in range(len(generation)):
+        parent1 = elites[random.randint(0, len(elites) - 1)]
+        parent2 = elites[random.randint(0, len(elites) - 1)]
+        next_generation.append(breed_routes(parent1, parent2))
+    return next_generation
 
 
+# Finding the best route in a generation
 def find_best_route(generation):
-    best_score = 1e20
+    best_score = float('inf')
     best_route = []
-    for i in range(len(generation)):
-        curr_score = calculateTotalDistance(generation[i])
-        if curr_score < best_score:
-            best_score = curr_score
-            best_route = generation[i]
+    for route in generation:
+        current_score = calculate_total_distance(route, x_coords, y_coords)
+        if current_score < best_score:
+            best_score = current_score
+            best_route = route
     return best_route, best_score
 
 
-def create_routes(number_of_routes):
+# Creating initial routes
+def create_initial_routes(number_of_routes, number_of_cities):
     routes = []
-    for i in range(number_of_routes):
-        routes.append(createRandomRoute())
+    for _ in range(number_of_routes):
+        routes.append(generate_random_route(number_of_cities))
     return routes
 
 
-# distance between city number 100 and city number 105
-# dist= distancebetweenCities(x[100], y[100], x[105], y[105])
-# print('Distance, % target: ', dist)
+# Main program
+if __name__ == "__main__":
+    cities_data = read_cities_data('TSPcities1000.txt')
+    x_coords = cities_data[1]
+    y_coords = cities_data[2]
 
-number_of_citites = 10
-size = 200
-epochs = 10
-current_gen = create_routes(size)
-best_score_progress = []  # Tracks progress
+    number_of_cities = 50
+    population_size = 500
+    epochs = 75
 
-best_route, best_score = find_best_route(current_gen)
-best_score_progress.append(best_score)
-print('Starting best score, % target: ', best_score)
-for i in range(epochs):
-    current_gen = crossover(current_gen)
-    best_route, best_score = find_best_route(current_gen)
+    current_generation = create_initial_routes(population_size, number_of_cities)
+    best_score_progress = []
+
+    best_route, best_score = find_best_route(current_generation)
+    plot_city_route(best_route, x_coords, y_coords)
     best_score_progress.append(best_score)
-    print(i, best_score)
+    print('Starting best score:', best_score)
 
-best_route, best_score = find_best_route(current_gen)
-# GA has completed required generation
-print('End best score, % target: ', best_score)
+    for i in range(epochs):
+        current_generation = crossover(current_generation)
+        best_route, best_score = find_best_route(current_generation)
+        best_score_progress.append(best_score)
+        print(i, best_score)
 
-plotCityRoute(best_route, 0, 100)
+    print('End best score:', best_score)
 
-plt.plot(best_score_progress)
-plt.xlabel('Generation')
-plt.ylabel('Best Fitness - route length - in Generation')
-# plt.show()
-plt.show()
+    plot_city_route(best_route, x_coords, y_coords)
+    plt.show()
+
+    plt.plot(best_score_progress)
+    plt.xlabel('Generation')
+    plt.ylabel('Best Fitness (Route Length)')
+    plt.show()
